88a1615376a8099494209fadb90ca3a88f9216e6
Matthias Ngeo <matthiasngeo@gmail.com>
2020-07-26 12:55:47 +0800

    Remove lingua

--- lingua/lib/lingua.dart
+++
@@ -1,8 +0,0 @@
-library lingua;
-
-import 'package:build/build.dart';
-import 'package:source_gen/source_gen.dart';
-
-import 'package:lingua/src/generator.dart';
-
-Builder build(BuilderOptions options) => SharedPartBuilder([LanguageGenerator(options.config)], 'lingua');
