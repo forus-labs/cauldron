5673b2b5f20a76a0c3c70a2af0351f83ff566170
Matthias <matthiasngeo@gmail.com>
2023-01-15 18:34:43 +0800

    Add boolean parsing

--- sugar/lib/src/core/booleans.dart
+++ sugar/lib/src/core/booleans.dart
@@ -1,7 +1,57 @@
 import 'package:meta/meta.dart';
+import 'package:sugar/core.dart';
+import 'package:sugar/math.dart'; // documentation
 
 /// Provides functions for working with booleans.
-extension Booleans on bool {
+extension Bools on bool {
+
+  /// Parses [source], case insensitive, as a [bool]. A [FormatException] is thrown if [source] cannot be parsed as a [bool].
+  ///
+  /// ```dart
+  /// Bools.parse('TRUE'); // true
+  /// Bools.parse('true'); // true
+  ///
+  /// Bools.parse('FALSE'); // false
+  /// Bools.parse('false'); // false
+  ///
+  /// Bools.parse('TRUE '); // throws FormatException
+  /// Bools.parse('0'); // throws FormatException
+  /// ```
+  @Possible({FormatException})
+  static bool parse(String source) {
+    final bool = tryParse(source);
+    if (bool != null) {
+      return bool;
+
+    } else {
+      throw FormatException('Invalid boolean', source);
+    }
+  }
+
+  /// Parses [source], case insensitive, as a [bool]. `null` is returned if [source] cannot be parsed as a [bool].
+  ///
+  /// ```dart
+  /// Bools.tryParse('TRUE'); // true
+  /// Bools.tryParse('true'); // true
+  ///
+  /// Bools.tryParse('FALSE'); // false
+  /// Bools.tryParse('false'); // false
+  ///
+  /// Bools.tryParse('TRUE '); // null
+  /// Bools.tryParse('0'); // null
+  /// ```
+  static bool? tryParse(String source) {
+    final formatted = source.toLowerCase();
+    if (formatted == 'true') {
+      return true;
+
+    } else if (formatted == 'false') {
+      return false;
+
+    } else {
+      return null;
+    }
+  }
 
   /// If `true`, returns `1`, otherwise returns `0`.
   ///
@@ -13,4 +63,8 @@ extension Booleans on bool {
   /// See [Integers.toBool].
   @useResult int toInt() => this ? 1 : 0;
 
+}
+
+void main() {
+  int.tryParse('value');
 }
--- sugar/test/src/core/booleans_test.dart
+++ sugar/test/src/core/booleans_test.dart
@@ -3,8 +3,37 @@ import 'package:test/test.dart';
 
 void main() {
   group('Booleans', () {
-    test('toInt() returns 1', () => expect(true.toInt(), 1));
+    for (final entry in {
+      'true': true,
+      'tRuE': true,
+      'false': false,
+      'fAlsE': false,
+    }.entries) {
+      test('parse(...)', () => expect(Bools.tryParse(entry.key), entry.value));
+    }
 
-    test('toInt() returns 0', () => expect(false.toInt(), 1));
+    for (final entry in {
+      'true ': null,
+      '0': null,
+    }.entries) {
+      test('parse(...) throws exception', () => expect(() => Bools.parse(entry.key), throwsFormatException));
+    }
+    
+    for (final entry in {
+      'true': true,
+      'tRuE': true,
+      'false': false,
+      'fAlsE': false,
+      'true ': null,
+      '0': null,
+    }.entries) {
+      test('tryParse(...)', () => expect(Bools.tryParse(entry.key), entry.value));
+    }
+
+    group('toInt()', () {
+      test('returns 1', () => expect(true.toInt(), 1));
+
+      test('returns 0', () => expect(false.toInt(), 0));
+    });
   });
 }
