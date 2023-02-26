836b16036fd1c6a42401f42a49ad8797ef99f2e5
Matthias <matthiasngeo@gmail.com>
2023-02-26 23:58:18 +0800

    Modify offset

--- sugar/lib/src/old/time/offset.dart
+++ sugar/lib/src/old/time/offset.dart
@@ -12,16 +12,15 @@ class Offset with Orderable<Offset> {
   /// This occurred in 1995 when the island of Kiribati moved its timezone eastward by one day which led to the creation
   /// of `+14:00`.
   static final Interval<Offset> range = Interval.closed(Offset(-18), Offset(18));
-  
-  static void 
-  
-  
-  final int _seconds;
 
-  
+  /// The hour of the offset, between `-18` to `18`, inclusive.
+  final int hour;
+  final int minute;
+  final int second;
+
   /// Creates an [Offset] for the current timezone.
   Offset.current(): this.fromDuration(DateTime.now().timeZoneOffset);
-  
+
   /// Creates an [Offset] from the given duration, with fractional seconds truncated.
   ///
   /// ```dart
@@ -31,8 +30,9 @@ class Offset with Orderable<Offset> {
   /// ### Contract:
   /// The given [duration] must be within the given [range]. A [RangeError] will otherwise be thrown.
   @Possible({RangeError})
-  Offset.fromDuration(Duration duration): _seconds = duration.inSeconds {
-    RangeError.checkValueInInterval(_seconds, -18 * Duration.secondsPerHour, 18 * Duration.secondsPerHour, 'duration');
+  Offset.fromDuration(Duration duration): second = duration.inSeconds {
+    DateTime.now().hour
+    RangeError.checkValueInInterval(second, -18 * Duration.secondsPerHour, 18 * Duration.secondsPerHour, 'duration');
   }
 
   /// Creates an [Offset] from the given total number of seconds.
@@ -44,7 +44,7 @@ class Offset with Orderable<Offset> {
   /// ### Contract:
   /// The given [seconds] must be within the given [range]. A [RangeError] will otherwise be thrown.
   @Possible({RangeError})
-  Offset.fromSeconds(int seconds): _seconds = seconds {
+  Offset.fromSeconds(int seconds): second = seconds {
     RangeError.checkValueInInterval(seconds, -18 * Duration.secondsPerHour, 18 * Duration.secondsPerHour, 'seconds');
   }
 
@@ -57,7 +57,7 @@ class Offset with Orderable<Offset> {
   /// ### Contract:
   /// The given arguments must be within the given [range]. A [RangeError] will otherwise be thrown.
   @Possible({RangeError})
-  Offset([int hour = 0, int minute = 0, int second = 0]): _seconds = Seconds.from(hour, minute, second) {
+  Offset([this.hour = 0, this.minute = 0, this.second = 0]) {
     RangeError.checkValueInInterval(hour, -18, 18, 'hour');
     RangeError.checkValueInInterval(minute, 0, 60, 'minute');
     RangeError.checkValueInInterval(hour, 0, 60, 'second');
@@ -65,22 +65,23 @@ class Offset with Orderable<Offset> {
 
 
   @Possible({RangeError})
-  Offset add({int hours = 0, int minutes = 0, int seconds = 0}) => Offset.fromSeconds(
-    _seconds + Seconds.from(hours, minutes, seconds),
-  );
+  Offset add({int hours = 0, int minutes = 0, int seconds = 0}) => Offset(
+    
+  )
+
+  // TODO: Offset.parse(...)
+
+  // TODO: addition & subtraction (operator on duration + on individual components)
+
+  // TODO: to duration
+
+  // TODO: in minutes/seconds?
+
+
+  @override
+  int compareTo(Offset other) => second.compareTo(other.second);
+
+  @override
+  int get hashValue => Object.hash(runtimeType, second);
   
-  @Possible({RangeError})
-  Offset substract({int hours = 0, int minutes = 0, int seconds = 0}) => Offset.fromSeconds(
-    _seconds - Seconds.from(hours, minutes, seconds),
-  );
-      
-  @override
-  int compareTo(Offset other) => _seconds.compareTo(other._seconds);
-
-  @override
-  int get hashValue => Object.hash(runtimeType, _seconds);
-
-  @override
-  String toString()
-
 }
