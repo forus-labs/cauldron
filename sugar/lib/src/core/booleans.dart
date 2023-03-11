b04c28d9a4808788a42125a5b8a8809d01b14d41
Matthias <matthiasngeo@gmail.com>
2023-03-11 20:37:33 +0800

    Remove boolean parse & tryParse
    These methods were added to the standard library in Dart 3

--- sugar/lib/src/core/booleans.dart
+++ sugar/lib/src/core/booleans.dart
@@ -5,55 +5,6 @@ import 'package:sugar/math.dart'; // documentation
 /// Provides functions for working with booleans.
 extension Bools on bool {
 
-  /// Parses [source], case insensitive, as a [bool]. A [FormatException] is thrown if [source] cannot be parsed as a [bool].
-  ///
-  /// ### Example:
-  /// ```dart
-  /// Bools.parse('TRUE'); // true
-  /// Bools.parse('true'); // true
-  ///
-  /// Bools.parse('FALSE'); // false
-  /// Bools.parse('false'); // false
-  ///
-  /// Bools.parse('TRUE '); // throws FormatException
-  /// Bools.parse('0'); // throws FormatException
-  /// ```
-  @Possible({FormatException})
-  static bool parse(String source) {
-    switch (source.toLowerCase()) {
-      case 'true':
-        return true;
-      case 'false':
-        return false;
-      default:
-        throw FormatException('Invalid boolean', source);
-    }
-  }
-
-  /// Parses [source], case insensitive, as a [bool]. `null` is returned if [source] cannot be parsed as a [bool].
-  ///
-  /// ### Example:
-  /// ```dart
-  /// Bools.tryParse('TRUE'); // true
-  /// Bools.tryParse('true'); // true
-  ///
-  /// Bools.tryParse('FALSE'); // false
-  /// Bools.tryParse('false'); // false
-  ///
-  /// Bools.tryParse('TRUE '); // null
-  /// Bools.tryParse('0'); // null
-  /// ```
-  static bool? tryParse(String source) {
-    switch (source.toLowerCase()) {
-      case 'true':
-        return true;
-      case 'false':
-        return false;
-      default:
-        return null;
-    }
-  }
-
   /// If `true`, returns `1`, otherwise returns `0`.
   ///
   /// ### Example:
--- sugar/test/src/core/booleans_test.dart
+++ sugar/test/src/core/booleans_test.dart
@@ -3,33 +3,6 @@ import 'package:test/test.dart';
 
 void main() {
   group('Booleans', () {
-    for (final entry in {
-      'true': true,
-      'tRuE': true,
-      'false': false,
-      'fAlsE': false,
-    }.entries) {
-      test('parse(...)', () => expect(Bools.tryParse(entry.key), entry.value));
-    }
-
-    for (final entry in {
-      'true ': null,
-      '0': null,
-    }.entries) {
-      test('parse(...) throws exception', () => expect(() => Bools.parse(entry.key), throwsFormatException));
-    }
-
-    for (final entry in {
-      'true': true,
-      'tRuE': true,
-      'false': false,
-      'fAlsE': false,
-      'true ': null,
-      '0': null,
-    }.entries) {
-      test('tryParse(...)', () => expect(Bools.tryParse(entry.key), entry.value));
-    }
-
     group('toInt()', () {
       test('returns 1', () => expect(true.toInt(), 1));
 
