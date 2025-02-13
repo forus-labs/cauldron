cca0546e0d6abb76cba23ed908f44244932b9885
Moshe Dicker <dickermoshe@gmail.com>
2025-02-13 10:00:35 -0500

    add java testing

---
+++ sugar/test/src/time/zone/providers/embedded/embedded_provider_test.dart
@@ -0,0 +1,141 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:collection/collection.dart';
+import 'package:jni/jni.dart';
+import 'package:path/path.dart';
+import 'package:sugar/src/time/zone/providers/embedded/embedded_timezone.dart';
+import 'package:sugar/sugar.dart';
+
+import 'package:test/test.dart';
+
+import '../known_timezones.dart';
+import 'java_provider/java_timezone_provider.dart';
+
+void main() {
+  Jni.spawn(dylibDir: join(Directory.current.path, 'build', 'jni_libs'));
+  final tests = <TestJob>[];
+  final javaProvider = JavaTimezoneProvider();
+  final embeddedProvider = EmbeddedTimezoneProvider();
+
+  group('EmbeddedTimezoneProvider', () {
+    test('containts known timezones', () {
+      /// All the known timezones should be in the embedded provider
+      expect(known.difference(embeddedProvider.keys.toSet()).length, 0);
+    });
+
+    final testTimezones = embeddedProvider.keys.toSet().intersection(javaProvider.keys.toSet());
+
+    for (final tz in testTimezones) {
+      final embeddedTz = embeddedProvider[tz]!;
+      final effectiveYears = _defaultYears(embeddedTz as EmbeddedTimezone).shuffled();
+      for (final year in effectiveYears) {
+        tests.add((tz: tz, year: year));
+      }
+    }
+    tests.shuffle();
+
+    group('embedded provider against java', () {
+      for (final t in tests) {
+        test('${t.tz} - ${t.year}', () {
+          final javaTz = javaProvider[t.tz]!;
+          final uniTz = embeddedProvider[t.tz]!;
+          expect(uniTz.name, javaTz.name);
+          var dt = DateTime.utc(t.year);
+          while (dt.year < t.year + 1) {
+            final uniSpan = uniTz.span(at: dt.microsecondsSinceEpoch);
+            final javaSpan = javaTz.span(at: dt.microsecondsSinceEpoch);
+            expect(
+              uniSpan.offset,
+              javaSpan.offset,
+              reason: 'Date: $dt, EmbeddedOffset:${uniSpan.offset}, JavaConverted:${javaSpan.offset}',
+            );
+            final localized = dt.add(Duration(microseconds: uniSpan.offset.inMicroseconds));
+            final javaConverted = javaTz.convert(
+              localized.year,
+              localized.month,
+              localized.day,
+              localized.hour,
+              localized.minute,
+              localized.second,
+              localized.millisecond,
+              localized.microsecond,
+            );
+            final uniConverted = uniTz.convert(
+              localized.year,
+              localized.month,
+              localized.day,
+              localized.hour,
+              localized.minute,
+              localized.second,
+              localized.millisecond,
+              localized.microsecond,
+            );
+
+            expect(
+              uniConverted.$1,
+              javaConverted.$1,
+              reason: 'Date: $localized, EmbeddedConverted:$uniConverted, JavaConverted:$javaConverted',
+            );
+            dt = dt.add(const Duration(minutes: 30));
+          }
+        });
+      }
+    });
+  });
+}
+
+typedef TestJob = ({String tz, int year});
+
+Set<int> _defaultYears(EmbeddedTimezone tz) {
+  final random = Random();
+
+  final years = {
+    -10,
+    10,
+    1700,
+    1800,
+    1900,
+    2000,
+    2100,
+
+    /// Pick a  3 random years between 1700 and 2100
+    /// and get the 14 possible years after it it
+    ...uniqueYears(random.nextInt(400) + 1700),
+    ...uniqueYears(random.nextInt(400) + 1700),
+    ...uniqueYears(random.nextInt(400) + 1700),
+  };
+
+  /// Test the years around the first and last transitions
+  /// to ensure that the provider is able to handle them
+  if (tz.lastYear case final int endYear) {
+    years
+      ..add(endYear - 1)
+      ..add(endYear)
+      ..add(endYear + 1);
+  }
+
+  if (tz.firstYear case final int firstYear) {
+    years
+      ..add(firstYear - 1)
+      ..add(firstYear)
+      ..add(firstYear + 1);
+  }
+  return years;
+}
+
+/// There are only 14 ways that a year can fall out
+/// 1. Starting weekday (7 possibilities)
+/// 2. Leap year or not (2 possibilities)
+/// This function returns a set of 14 unique years
+/// right after the start year
+Set<int> uniqueYears([int? startYear]) {
+  bool isLeapYear(int year) => (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+  final uniqueYears = <({int startDay, bool isLeapYear}), int>{};
+  var year = startYear ?? DateTime.now().year;
+  while (uniqueYears.length != 14) {
+    uniqueYears[(startDay: DateTime(year).weekday, isLeapYear: isLeapYear(year))] = year;
+    year++;
+  }
+  return uniqueYears.values.toSet();
+}
---
+++ sugar/test/src/time/zone/providers/embedded/java_provider/java_timezone_provider.dart
@@ -0,0 +1,96 @@
+import 'dart:collection';
+
+import 'package:jni/jni.dart';
+import 'package:sugar/src/time/offset.dart';
+import 'package:sugar/src/time/temporal_unit.dart';
+import 'package:sugar/src/time/zone/timezone.dart';
+import 'package:sugar/time_zone.dart';
+
+import 'bindings.dart';
+
+class JavaTimezoneProvider extends UnmodifiableMapBase<String, Timezone> {
+  final _cache = <String, Timezone>{};
+  @override
+  Timezone? operator [](Object? key) {
+    if (key is String && keys.contains(key)) {
+      if (_cache.containsKey(key)) {
+        return _cache[key];
+      }
+      final timezone = JavaTimezone(key);
+      _cache[key] = timezone;
+      return timezone;
+    }
+    return null;
+  }
+
+  @override
+  late final keys = ZoneId.getAvailableZoneIds()!.use(
+    (ids) => ids.map((element) => element?.toDartString(releaseOriginal: true)).nonNulls.toSet(),
+  );
+}
+
+/// A timezone that uses the Java timezone database.
+class JavaTimezone extends Timezone {
+  /// Creates a new Java timezone with the given [name].
+  JavaTimezone(super.name) : super.from();
+
+  @override
+  TimezoneSpan span({required EpochMicroseconds at}) {
+    final instant = Instant.ofEpochMilli(at ~/ 1000);
+    final result = ZonedDateTime.ofInstant(
+      instant,
+      _zoneId,
+    )!.use((p0) => p0.getOffset()!.use((offset) => offset.getTotalSeconds()));
+    instant!.release();
+    return JavaTimezoneSpan(offset: Offset.fromSeconds(result), abbreviation: null, dst: false);
+  }
+
+  /// TODO: When we move this to the main library, we should remove
+  /// this so that we don't have memory leaks. We should instead create
+  /// a new zodeId object each time we need it.
+  late final _zoneId = JString.fromString(name).use(ZoneId.of$1)!;
+
+  @override
+  (EpochMicroseconds, TimezoneSpan) convert(
+    int year, [
+    int month = 1,
+    int day = 1,
+    int hour = 0,
+    int minute = 0,
+    int second = 0,
+    int millisecond = 0,
+    int microsecond = 0,
+  ]) {
+    final nanoSeconds = (microsecond * 1000) + (millisecond * 1000000);
+
+    final micros = ZonedDateTime.of$2(year, month, day, hour, minute, second, nanoSeconds, _zoneId)!.use(
+      (zonedDateTime) =>
+          Instant.from(zonedDateTime)!.use((instant) => instant.getEpochSecond() * Duration.microsecondsPerSecond),
+    );
+    return (micros, span(at: micros));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    return other is JavaTimezone && other.name == name;
+  }
+
+  @override
+  int get hashCode => name.hashCode;
+}
+
+class JavaTimezoneSpan extends TimezoneSpan {
+  /// Creates a new instance of [JavaTimezoneSpan].
+  JavaTimezoneSpan({required super.offset, required super.abbreviation, required super.dst});
+
+  /// The start of the span.
+  @override
+  EpochMicroseconds? get start => null;
+
+  /// The end of the span.
+  @override
+  EpochMicroseconds? get end => null;
+}
